9652f24bde1f1e44a5d255619f6ec94a41e6594d
dong002 <81304010+dong002@users.noreply.github.com>
2022-12-28 14:37:47 +0900

    done

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
-const bodyParser = require('body-parser')
-const ssExpress = require('sse-express')
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/connect',ssExpress(),(req,res)=>{
+app.get('/status', (request, response) => response.json({ clients: clients.length }));
 
-    // res.sse({
-    //     event: 'connected',
-    //     data: {
-    //         welcomeMsg: 'Hello world'
+const PORT = 3000;
 
-    //     }
-    // })
-    app.on('hello world', data => {
-        res.sse({
-            event: 'hello world',
-            data: data
-        })
-    })
+let clients = [];
+let livestreamEvents = [];
+
+app.listen(PORT, () => {
+    console.log(`Livestream service starting`)
 })
 
-app.post('/message', (req, res, next) => {
-    const message = req.body.message;
-    res.send({msg: 'ok'})
+function eventsHandler(request, response, next) {
+    const headers = {
+      'Content-Type': 'text/event-stream',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+  
+    const data = 'connect'//`data: ${JSON.stringify(livestreamEvents)}\n\n`;
+  
+    response.write(data);
+  
+    const clientId = Date.now();
+  
+    const newClient = {
+      id: clientId,
+      response
+    };
+  
+    clients.push(newClient);
+  
+    request.on('close', () => {
+      console.log(clients);
+      //clients = clients.filter(client => client.id !== clientId);
+    });
+  }
+  
+app.get('/events', eventsHandler);
 
-    app.emit('hello world', {
-        title: 'hello world',
-        message,
-        timestamp: new Date()
-    })
-})
+function sendEventsToAll(livestreamEvent) {
+    clients.forEach(client => client.response.write(`data: ${JSON.stringify(livestreamEvent)}\n\n`))
+}
+
+async function addLivestreamEvent(request, respsonse, next) {
+    const livestreamEvent = request.body;
+    livestreamEvents.push(livestreamEvent);
+    respsonse.json(livestreamEvent)
+    return sendEventsToAll(livestreamEvent);
+}
+
+app.post('/event', addLivestreamEvent);
